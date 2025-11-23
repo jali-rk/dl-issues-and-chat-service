f2a3f6059837e4a23e2b275815b7439b592d43ec
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2025-11-23 10:06:13 +0530

    Initialize service

---
+++ src/main/java/com/dopaminelite/dl_issues_and_chat_service/DlIssuesAndChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dopaminelite.dl_issues_and_chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlIssuesAndChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlIssuesAndChatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dopaminelite/dl_issues_and_chat_service/DlIssuesAndChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dopaminelite.dl_issues_and_chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlIssuesAndChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
